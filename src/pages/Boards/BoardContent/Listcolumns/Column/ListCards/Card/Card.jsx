af23c25f02e6e94fed9edf5119f92fd4a9d851e8
mQhuy193 <mQhuy1903@gmail.com>
2025-11-02 20:00:44 +0700

    trigger for on drag over event

--- src/pages/Boards/BoardContent/Listcolumns/Column/ListCards/Card/Card.jsx
+++ src/pages/Boards/BoardContent/Listcolumns/Column/ListCards/Card/Card.jsx
@@ -20,7 +20,8 @@ function TrelloCard({ card }) {
     // touchAction: 'none',
     transform: CSS.Translate.toString(transform),
     transition,
-    opacity: isDragging ? 0.5 : undefined
+    opacity: isDragging ? 0.5 : undefined,
+    border: isDragging ? '1px solid #2ecc71' : undefined
   }
 
   const shouldShowCardActions = () => {
